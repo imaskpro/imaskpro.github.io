37cffc6d3b95ae9481c9f9ac9cd89b4a4ebddd09
imaskpro <73496466+imaskpro@users.noreply.github.com>
2024-12-14 09:28:08 +0700

    zz

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 rm ./Packages.bz2
 dpkg-scanpackages -m ./debs > Packages
+cp -f Packages Packages_old
 bzip2 Packages
+cp -f Packages_old Packages
 git add .
 git commit -m "zz"
 git push origin main                

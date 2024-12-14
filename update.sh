4cbe62eb8bba2344996ae03ae167a001b4e1a34e
imaskpro <73496466+imaskpro@users.noreply.github.com>
2024-12-14 07:51:48 +0700

    sdfgsgdsg

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-rm Packages Packages.bz2
+# Remove old package files
+rm -f Packages Packages.bz2 Packages.gz
+
+# Generate a new Packages file
 dpkg-scanpackages -m ./debs > Packages
-bzip2 -fks Packages
+
+# Compress the Packages file into .bz2 and .gz formats
+bzip2 -fks Packages  # Create Packages.bz2
+gzip -fk Packages    # Create Packages.gz
+
+echo "Packages, Packages.bz2, and Packages.gz have been generated successfully."
